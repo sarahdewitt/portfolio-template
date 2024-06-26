557380535163b1d271940bf6cd7cf3d7c10cd120
sarah de witt <sarahdewitt15@gmail.com>
2024-06-26 13:04:09 +0800

    fix: fixed smooth scroll with lenis and container animations

--- components/atoms/Container.tsx
+++ components/atoms/Container.tsx
@@ -12,9 +12,9 @@ export default function Container({
     <motion.div
       className="border-b-[1px] border-gray px-4 py-14 md:py-32" 
       id={id}
-      // initial={{ opacity: 0, y: 100 }}
-      // whileInView={{ opacity: 1, y: 0 }}
-      // transition={{ duration: 0.5, delay: 0.25, ease: "easeInOut" }}
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: 0.25, ease: "easeInOut" }}
     >
       {children}
     </motion.div>
